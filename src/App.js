e188a9c52993872bc85d15dc8bb8e24e5e1fb0ae
Invoker~ <janvincent.claudio@gmail.com~>
2022-10-25 01:04:02 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './app.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { addUser } from './features/reducer/Users';
 
 function App() {
+  const [name, setName] = useState("");
+  const [username, setUserame] = useState("");
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      {" "}
+      <div className={styles.addUser}>
+        <input type="text" placehpler= "Enter Name ...." onChange={(e) => {setName(e.target.value)}}/>
+        <input type="text" placehpler="Enter Username ...." onChange={(e) => {setUserame(e.target.value)}}/>
+        <button onClick={() => {dispatch(addUser({id: 0, name, username}))}}>Add User</button>
+      </div>
+      <div className={styles.displayUser}>
+        {userList.map((users) => {
+          return (
+            <>
+              <div key={users.id}>
+                <h1>{users.name}</h1>
+                <h1>{users.username}</h1>
+              </div>
+            </>
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,47 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Jan Vincent",
+    username: "Js_Invoker",
+  },
+  {
+    id: 2,
+    name: "Christian",
+    username: "Js_Inchang",
+  },
+  {
+    id: 3,
+    name: "Brent",
+    username: "Js_Stiles",
+  },
+  {
+    id: 4,
+    name: "Bryan Ja,es",
+    username: "Js_Jamie",
+  },
+  {
+    id: 5,
+    name: "Kazuyoshi",
+    username: "Js_Yoshi",
+  },
+  {
+    id: 6,
+    name: "Zeus",
+    username: "Js_Apex",
+  },
+  {
+    id: 7,
+    name: "John Karl",
+    username: "Js_Jangkal",
+  },
+  {
+    id: 8,
+    name: "Wilhelm",
+    username: "Js_Wilhelm",
+  },
+  {
+    id: 9,
+    name: "Matthew",
+    username: "Js_Skadi",
+  },
+];
